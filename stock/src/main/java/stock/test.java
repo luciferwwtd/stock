67dbc743ed04c9d114dae05933f5972e9d374854
seanl <seanl@sean.lan>
2023-08-26 19:45:10 +0900

    뭐

---
+++ stock/src/main/java/stock/test.java
@@ -0,0 +1,12 @@
+package stock;
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String a = "A";
+		String b = "B";
+		System.out.printf("%s %s\n", a, b);
+	}
+
+}
