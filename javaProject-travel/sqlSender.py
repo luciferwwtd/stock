3dbbb87156fe52273788cadcb190f6db309a6c13
Lucifer <116432517+luciferwwtd@users.noreply.github.com>
2024-07-05 11:45:13 +0900

    Update sqlSender.py

--- javaProject-travel/sqlSender.py
+++ javaProject-travel/sqlSender.py
@@ -21,9 +21,6 @@ class MyWindow(QMainWindow):
 
         self.descEdit = QLineEdit("", self)
         self.descEdit.move(20, 50)
-
-        self.linkEdit = QLineEdit("", self)
-        self.linkEdit.move(20, 50)
  
         button2 = QLineEdit("전송하기", self)
         button2.move(130, 50)
@@ -38,11 +35,9 @@ class MyWindow(QMainWindow):
         charset='utf8')
       try:
         with conn.cursor() as cursor:
-          sql = "INSERT INTO travel (place, name, desc, link) VALUES ('%s', '%s', '%s', '%s')" % (placeEdit.text(), nameEdit.text(), descEdit.text(), linkEdit.text())
+          sql = "INSERT INTO travel (place, name, desc) VALUES ('%s', '%s', '%s')" % (placeEdit.text(), nameEdit.text(), descEdit.text())
           print(sql)
           cursor.execute(sql)
-          # sql = 'INSERT INTO users (email, password) VALUES (%s, %s)'
-          # cursor.execute(sql, ('test@test.com', 'my-passwd'))
         conn.commit()
         print(cursor.lastrowid)
         # 1 (last insert id)
